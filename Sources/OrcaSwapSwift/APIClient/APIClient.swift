fccdabd8960a47f9db38b751e5ebc9480107f562
Chung Tran <bigearsenal@gmail.com>
2022-05-06 10:27:05 +0700

    feat: APIClientV2

--- Sources/OrcaSwapSwift/APIClient/APIClient.swift
+++ Sources/OrcaSwapSwift/APIClient/APIClient.swift
@@ -8,16 +8,16 @@
 import Foundation
 import RxSwift
 
-public protocol OrcaSwapAPIClient {
+public protocol OrcaSwapAPIClientV2 {
     var network: String {get}
-    func reload() -> Completable
-    func getTokens() -> Single<[String: TokenValue]>
-    func getAquafarms() -> Single<[String: Aquafarm]>
-    func getPools() -> Single<[String: Pool]>
-    func getProgramID() -> Single<ProgramIDS>
+    func reload() async throws
+    func getTokens() async throws -> [String: TokenValue]
+    func getAquafarms() async throws -> [String: Aquafarm]
+    func getPools() async throws -> [String: Pool]
+    func getProgramID() async throws -> ProgramIDS
 }
 
-public class APIClient: OrcaSwapAPIClient {
+public class APIClientV2: OrcaSwapAPIClientV2 {
     // MARK: - Properties
     
     private let urlString = "https://api.orca.so/configs"
@@ -34,31 +34,30 @@ public class APIClient: OrcaSwapAPIClient {
     }
     
     // MARK: - Methods
-    public func reload() -> Completable {
-        getConfigs(reload: true)
-            .asCompletable()
+    public func reload() async throws {
+        _ = try await getConfigs(reload: true)
     }
     
-    public func getTokens() -> Single<[String : TokenValue]> {
-        getConfigs().map {$0.tokens}
+    public func getTokens() async throws -> [String: TokenValue] {
+        try await getConfigs().tokens
     }
     
-    public func getAquafarms() -> Single<[String : Aquafarm]> {
-        getConfigs().map {$0.aquafarms}
+    public func getAquafarms() async throws -> [String: Aquafarm] {
+        try await getConfigs().aquafarms
     }
     
-    public func getPools() -> Single<[String : Pool]> {
-        getConfigs().map {$0.pools}
+    public func getPools() async throws -> [String: Pool] {
+        try await getConfigs().pools
     }
     
-    public func getProgramID() -> Single<ProgramIDS> {
-        getConfigs().map {$0.programIDS}
+    public func getProgramID() async throws -> ProgramIDS {
+        try await getConfigs().programIDS
     }
     
     // MARK: - Helpers
-    private func getConfigs(reload: Bool = false) -> Single<OrcaConfigs> {
+    private func getConfigs(reload: Bool = false) async throws -> OrcaConfigs {
         if !reload, let cache = cache {
-            return .just(cache)
+            return cache
         }
         // hack: network
         var network = network
@@ -68,14 +67,52 @@ public class APIClient: OrcaSwapAPIClient {
         let url = URL(string: urlString)!
         
         // get
-        return URLSession.shared.rx.data(request: .init(url: url))
-            .take(1)
-            .asSingle()
-            .observe(on: ConcurrentDispatchQueueScheduler(qos: .userInteractive))
-            .map { try JSONDecoder().decode(OrcaConfigs.self, from: $0) }
-            .do(onSuccess: { [weak self] configs in
-                self?.locker.lock(); defer {self?.locker.unlock()}
-                self?.cache = configs
-            })
+        let (data, _): (Data, URLResponse)
+        if #available(iOS 15.0, macOS 12.0, *) {
+            (data, _) = try await URLSession.shared.data(for: .init(url: url))
+        } else {
+            (data, _) = try await URLSession.shared.data(from: url)
+        }
+        let configs = try JSONDecoder().decode(OrcaConfigs.self, from: data)
+        
+        locker.lock()
+        cache = configs
+        locker.unlock()
+        
+        return configs
     }
+}
+
+@available(iOS, deprecated: 15.0, message: "Use the built-in API instead")
+extension URLSession {
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: url) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+
+                continuation.resume(returning: (data, response))
+            }
+
+            task.resume()
+        }
+    }
+    
+    func data(from urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: urlRequest) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+
+                continuation.resume(returning: (data, response))
+            }
+
+            task.resume()
+        }
+    }
+
 }
